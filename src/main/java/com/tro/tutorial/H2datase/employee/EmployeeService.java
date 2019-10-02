2aa11ae694d0841d660a10dd267e2fce2a8a3ed9
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2019-10-01 22:40:57 -0400

    update api for objext Rest service added

--- src/main/java/com/tro/tutorial/H2datase/employee/EmployeeService.java
+++ src/main/java/com/tro/tutorial/H2datase/employee/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.tro.tutorial.H2datase.employee;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeService {
 
-		private List<Employee> employees = Arrays.asList(
+		private List<Employee> employees = new ArrayList <> ( 
+				Arrays.asList(
 				new Employee("as", "Ahmed", "Sr JEE"),
 				new Employee("os", "Omar", "Toddler"),
 				new Employee("ts", "Tiba", "Kid")
-				);
+				));
 	
 	public List<Employee> getAllEmployees(){
 		return this.employees;
@@ -20,5 +22,21 @@ public class EmployeeService {
 
 	public Employee getEmployee(String id) {
 		return employees.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+		
+	}
+
+	public void updateEmployee(String id, Employee employee) {
+		for (int i = 0; i < employees.size(); i++){
+			Employee e = employees.get(i);
+			if(e.getId().equals(id)){
+				employees.set(i, employee);
+				return;
+			}
+		}
+		
 	}
 }
--- src/main/java/com/tro/tutorial/H2datase/employee/WebContainer.java
+++ src/main/java/com/tro/tutorial/H2datase/employee/WebContainer.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +27,18 @@ public class WebContainer {
 	public Employee findEmployee(@PathVariable String id){
 			return employeeService.getEmployee(id);
 	}
+	
+	//@RequestMapping(value="/employees",method=RequestMethod.POST )
+	@PostMapping("/employees")
+	public void addEmployee(@RequestBody Employee employee){
+		employeeService.addEmployee(employee);
+	}
+	
+	@RequestMapping(value="/employees/{id}",method=RequestMethod.PUT)
+	public void updateEmployee(@RequestBody Employee employee, @PathVariable String id){
+		employeeService.updateEmployee(id,employee);
+	}
+	
+	
 
 }
